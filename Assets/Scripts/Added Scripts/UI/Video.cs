3c225a31eecac2810b9426f57e45537ccc10eedf
Heather <35993724+HeatherCochrane@users.noreply.github.com>
2020-01-26 23:21:38 +0000

    FinalSubmit
    Final commit before deadline

--- Assets/Scripts/Added Scripts/UI/Video.cs
+++ Assets/Scripts/Added Scripts/UI/Video.cs
@@ -16,6 +16,7 @@ public class Video : MonoBehaviour
 
 		menu.transform.GetChild(0).transform.gameObject.SetActive(false);
 		menu.transform.GetChild(1).transform.gameObject.SetActive(false);
+		menu.transform.GetChild(2).transform.gameObject.SetActive(false);
 		menu.color = Color.black;
 		
 		Invoke("intro", 8);
@@ -30,6 +31,7 @@ public class Video : MonoBehaviour
 	{
 		menu.transform.GetChild(0).transform.gameObject.SetActive(true);
 		menu.transform.GetChild(1).transform.gameObject.SetActive(true);
+		menu.transform.GetChild(2).transform.gameObject.SetActive(true);
 
 		menu.color = new Color32(178, 210, 228, 225);
 		Destroy(this.gameObject);
